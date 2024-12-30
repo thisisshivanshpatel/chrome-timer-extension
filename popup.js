9ecabc9b20925ee58df34d5855e0d9bc3d332105
shivansh patel <shivanshpatel.1999@gmail.com>
2024-12-31 00:13:18 +0530

    Refactor timer input layout and enhance styling for better user experience and domcontent loaded check added

--- popup.js
+++ popup.js
@@ -1,50 +1,53 @@
 let timers = [];
 
-const startButton = document.getElementById("start");
-const timersContainer = document.getElementById("timers");
+document.addEventListener("DOMContentLoaded", function () {
+  const startButton = document.getElementById("start");
+  const timersContainer = document.getElementById("timers");
 
-startButton.addEventListener("click", function () {
-  const hours = parseInt(document.getElementById("hours")?.value) || 0;
-  const minutes = parseInt(document.getElementById("minutes")?.value) || 0;
-  const seconds = parseInt(document.getElementById("seconds")?.value) || 0;
+  startButton.addEventListener("click", function () {
+    const hours = parseInt(document.getElementById("hours")?.value) || 0;
+    const minutes = parseInt(document.getElementById("minutes")?.value) || 0;
+    const seconds = parseInt(document.getElementById("seconds")?.value) || 0;
 
-  if (hours) {
-    document.getElementById("hours").value = null;
-  }
+    if (hours) {
+      document.getElementById("hours").value = null;
+    }
 
-  if (minutes) {
-    document.getElementById("minutes").value = null;
-  }
+    if (minutes) {
+      document.getElementById("minutes").value = null;
+    }
 
-  if (seconds) {
-    document.getElementById("seconds").value = null;
-  }
+    if (seconds) {
+      document.getElementById("seconds").value = null;
+    }
 
-  const timeLeft = hours * 3600 + minutes * 60 + seconds;
-  const timerId = timers.length;
+    if (hours || minutes || seconds) {
+      const timeLeft = hours * 3600 + minutes * 60 + seconds;
+      const timerId = timers.length;
 
-  const timer = {
-    id: timerId,
-    timeLeft: timeLeft,
-    interval: null,
-    isRunning: true,
-  };
+      const timer = {
+        id: timerId,
+        timeLeft: timeLeft,
+        interval: null,
+        isRunning: true,
+      };
 
-  timers.push(timer);
-  saveTimers();
-  renderTimer(timer);
-  startTimer(timer);
-});
+      timers.push(timer);
+      saveTimers();
+      renderTimer(timer);
+      startTimer(timer);
+    }
+  });
 
-function renderTimer(timer) {
-  const timerElement = document.createElement("div");
-  timerElement.className = "timer-container";
-  timerElement.id = `timer-${timer.id}`;
+  function renderTimer(timer) {
+    const timerElement = document.createElement("div");
+    timerElement.className = "timer-container";
+    timerElement.id = `timer-${timer.id}`;
 
-  timerElement.innerHTML = `
+    timerElement.innerHTML = `
         <p id="display-${timer.id}" class="time">${formatTime(
-    timer.timeLeft
-  )}</p>
+      timer.timeLeft
+    )}</p>
         <div class="timer-controls">
             <img src="icons/pause.png" height="22" width="22" class="playPause" id="pause-${
               timer.id
@@ -58,91 +61,96 @@ function renderTimer(timer) {
         </div>
     `;
 
-  timersContainer.appendChild(timerElement);
+    timersContainer.appendChild(timerElement);
 
-  document
-    .getElementById(`pause-${timer.id}`)
-    .addEventListener("click", function () {
-      pauseTimer(timer);
-    });
-
-  document
-    .getElementById(`play-${timer.id}`)
-    .addEventListener("click", function () {
-      playTimer(timer);
-    });
-
-  document
-    .getElementById(`cancel-${timer.id}`)
-    .addEventListener("click", function () {
-      cancelTimer(timer);
-    });
-}
-
-function startTimer(timer) {
-  timer.interval = setInterval(function () {
-    if (timer.timeLeft <= 0) {
-      clearInterval(timer.interval);
-      document.getElementById(`display-${timer.id}`).textContent = "00:00:00";
-      alert("Time is up!");
-    } else {
-      timer.timeLeft--;
-      document.getElementById(`display-${timer.id}`).textContent = formatTime(
-        timer.timeLeft
-      );
-    }
-    saveTimers();
-  }, 1000);
-}
-
-function pauseTimer(timer) {
-  clearInterval(timer.interval);
-  timer.isRunning = false;
-  document.getElementById(`pause-${timer.id}`).style.display = "none";
-  document.getElementById(`play-${timer.id}`).style.display = "inline";
-  saveTimers();
-}
-
-function playTimer(timer) {
-  startTimer(timer);
-  timer.isRunning = true;
-  document.getElementById(`pause-${timer.id}`).style.display = "inline";
-  document.getElementById(`play-${timer.id}`).style.display = "none";
-  saveTimers();
-}
-
-function cancelTimer(timer) {
-  clearInterval(timer.interval);
-  document.getElementById(`timer-${timer.id}`).remove();
-  timers = timers.filter((t) => t.id !== timer.id);
-  saveTimers();
-}
-
-function formatTime(seconds) {
-  const hours = Math.floor(seconds / 3600);
-  const minutes = Math.floor((seconds % 3600) / 60);
-  const secs = seconds % 60;
-  return `${hours.toString().padStart(2, "0")}:${minutes
-    .toString()
-    .padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
-}
-
-function saveTimers() {
-  chrome.storage.local.set({ timers: timers });
-}
-
-function loadTimers() {
-  chrome.storage.local.get("timers", (result) => {
-    if (result.timers) {
-      timers = Array.isArray(result.timers) ? result.timers : [];
-      timers.forEach((timer) => {
-        renderTimer(timer);
-        if (timer.isRunning) {
-          startTimer(timer);
-        }
+    document
+      .getElementById(`pause-${timer.id}`)
+      .addEventListener("click", function () {
+        pauseTimer(timer);
       });
-    }
-  });
-}
 
-document.addEventListener("DOMContentLoaded", loadTimers);
+    document
+      .getElementById(`play-${timer.id}`)
+      .addEventListener("click", function () {
+        playTimer(timer);
+      });
+
+    document
+      .getElementById(`cancel-${timer.id}`)
+      .addEventListener("click", function () {
+        cancelTimer(timer);
+      });
+  }
+
+  function startTimer(timer) {
+    timer.interval = setInterval(function () {
+      if (timer.timeLeft <= 0) {
+        clearInterval(timer.interval);
+        document.getElementById(`display-${timer.id}`).textContent = "00:00:00";
+        cancelTimer(timer);
+        alert("Time is up!");
+      } else {
+        timer.timeLeft--;
+        document.getElementById(`display-${timer.id}`).textContent = formatTime(
+          timer.timeLeft
+        );
+      }
+      saveTimers();
+    }, 1000);
+  }
+
+  function pauseTimer(timer) {
+    clearInterval(timer.interval);
+    timer.isRunning = false;
+    document.getElementById(`pause-${timer.id}`).style.display = "none";
+    document.getElementById(`play-${timer.id}`).style.display = "inline";
+    saveTimers();
+  }
+
+  function playTimer(timer) {
+    startTimer(timer);
+    timer.isRunning = true;
+    document.getElementById(`pause-${timer.id}`).style.display = "inline";
+    document.getElementById(`play-${timer.id}`).style.display = "none";
+    saveTimers();
+  }
+
+  function cancelTimer(timer) {
+    clearInterval(timer.interval);
+    document.getElementById(`timer-${timer.id}`).remove();
+    timers = timers.filter((t) => t.id !== timer.id);
+    saveTimers();
+  }
+
+  function formatTime(seconds) {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const secs = seconds % 60;
+    return `${hours
+      .toString()
+      .padStart(
+        2,
+        "0"
+      )}:${minutes.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
+  }
+
+  function saveTimers() {
+    chrome.storage.local.set({ timers: timers });
+  }
+
+  function loadTimers() {
+    chrome.storage.local.get("timers", (result) => {
+      if (result.timers) {
+        timers = Array.isArray(result.timers) ? result.timers : [];
+        timers.forEach((timer) => {
+          renderTimer(timer);
+          if (timer.isRunning) {
+            startTimer(timer);
+          }
+        });
+      }
+    });
+  }
+
+  loadTimers();
+});
